625175e675f4c28bf9051896f3400c5967357dc1
lytranuit@gmail.com <lytranuit@gmail.com>
2021-10-25 08:48:53 +0700

    import document truongqa

--- app/Controllers/Admin/Import.php
+++ app/Controllers/Admin/Import.php
@@ -649,6 +649,160 @@ class Import extends BaseController
             }
         }
     }
+    public function truongqa()
+    {
+        //Đường dẫn file
+        $file = APPPATH . '../assets/up/List & Database-SPC.xlsx';
+
+        /** Load $inputFileName to a Spreadsheet Object  **/
+        // $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($file);
+        // print_r($spreadsheet);
+        // die();
+        //Tiến hành xác thực file
+        $objFile = \PhpOffice\PhpSpreadsheet\IOFactory::identify($file);
+        $objData = \PhpOffice\PhpSpreadsheet\IOFactory::createReader($objFile);
+
+        //Chỉ đọc dữ liệu
+        // $objData->setReadDataOnly(true);
+        // Load dữ liệu sang dạng đối tượng
+        $objPHPExcel = $objData->load($file);
+        //Lấy ra số trang sử dụng phương thức getSheetCount();
+        // Lấy Ra tên trang sử dụng getSheetNames();
+        //Chọn trang cần truy xuất
+        // $sheet = $objPHPExcel->setActiveSheetIndex(0);
+
+        // //Lấy ra số dòng cuối cùng
+        // $Totalrow = $sheet->getHighestRow();
+        // //Lấy ra tên cột cuối cùng
+        // $LastColumn = $sheet->getHighestColumn();
+        // //Chuyển đổi tên cột đó về vị trí thứ, VD: C là 3,D là 4
+        // $TotalCol = \PhpOffice\PhpSpreadsheet\Cell\Coordinate::columnIndexFromString($LastColumn);
+
+        //Tạo mảng chứa dữ liệu
+        $data = [];
+
+        $count_sheet = $objPHPExcel->getSheetCount();
+        // print_r($count_sheet);
+        // die();
+        for ($k = 0; $k < $count_sheet; $k++) {
+
+            $sheet = $objPHPExcel->setActiveSheetIndex($k);
+            $sheet_name = $sheet->getTitle();
+            // if (strpos($sheet_name, "#") == false) continue;
+            // print_r($sheet_name);die();
+            //Lấy ra số dòng cuối cùng
+            $Totalrow = $sheet->getHighestRow();
+            //Lấy ra tên cột cuối cùng
+            $LastColumn = $sheet->getHighestColumn();
+            //Chuyển đổi tên cột đó về vị trí thứ, VD: C là 3,D là 4
+            $TotalCol = \PhpOffice\PhpSpreadsheet\Cell\Coordinate::columnIndexFromString($LastColumn);
+
+            //Tạo mảng chứa dữ liệu
+            $data = [];
+
+            //Tiến hành lặp qua từng ô dữ liệu
+            //----Lặp dòng, Vì dòng đầu là tiêu đề cột nên chúng ta sẽ lặp giá trị từ dòng 2
+            $row = 4;
+            for ($i = $row; $i <= $Totalrow; $i++) {
+                //----Lặp cột
+                for ($j = 0; $j < $TotalCol; $j++) {
+                    // Tiến hành lấy giá trị của từng ô đổ vào mảng
+                    $cell = $sheet->getCellByColumnAndRow($j, $i);
+
+                    $data[$i -  $row][$j] = $cell->getValue();
+                    ///CHUYEN RICH TEXT
+                    if ($data[$i -  $row][$j] instanceof \PhpOffice\PhpSpreadsheet\RichText\RichText) {
+                        $data[$i -  $row][$j] = $data[$i -  $row][$j]->getPlainText();
+                    }
+
+                    // ////CHUYEN DATE 
+                    // if (PHPExcel_Shared_Date::isDateTime($cell) && $data[$i - 1][$j] > 0) {
+
+                    //     if (is_numeric($data[$i - 1][$j])) {
+                    //         $data[$i - 1][$j] = date("Y-m-d", PHPExcel_Shared_Date::ExcelToPHP($data[$i - 1][$j]));
+                    //     } else if ($data[$i - 1][$j] == '26/09/16') {
+                    //         $data[$i - 1][$j] = '2016-09-26';
+                    //     }
+                    // }
+                }
+            }
+
+
+            echo "<pre>";
+            // echo $sheet_name . "<br>";
+            // print_r($data);
+            // die();
+            $document_model = model("DocumentModel");
+            foreach ($data as $row) {
+
+                $name = $row[3];
+                if ($name == "") continue;
+                // $row[1] = trim($row[1]);
+                // $explode =  explode(".", $row[1]);
+                // if (count($explode) < 2) continue;
+
+                // $version = $row[2];
+                $code = $row[2];
+                // $is_active = $row[9];
+                $description = $row[13];
+
+                // $other = strlen($version);
+                $explode = array();
+                if ($code != "" && $code != "NA") {
+                    $explode =  explode("-", $code);
+                    if (count($explode) >= 2) {
+                        $version = $explode[count($explode) - 1];
+                        $code = substr($code, 0, 0 - strlen($version) - 1);
+                    }
+                }
+
+
+                $array = preg_split("/\r\n|\n|\r/", $name);
+                $name_vi = isset($array[0]) ? $array[0] : "";
+                $name_en = isset($array[1]) ? $array[1] : "";
+                // print_r($array);
+                // die();
+                $date_effect_row = explode(".", $row[4]);
+                // // $date_review_row = explode(".", $row[5]);
+                if (count($date_effect_row) < 3) {
+                    $date_effect = null;
+                } else {
+                    $d = $date_effect_row[0];
+                    $m = $date_effect_row[1];
+                    $y = "20$date_effect_row[2]";
+                    $date_effect = "$y-$m-$d";
+                }
+                // if (count($date_review_row) < 3) {
+                //     $date_review = null;
+                // } else {
+                //     $d = $date_review_row[0];
+                //     $m = $date_review_row[1];
+                //     $y = "20$date_review_row[2]";
+                //     $date_review = "$y-$m-$d";
+                // }
+                $status_id = 2;
+
+                $type_id = 10;
+                $is_active = 1;
+
+                $array = array(
+                    // 'other' => $explode,
+                    'code' => $code,
+                    'version' => $version,
+                    'date_effect' => $date_effect,
+                    'name_vi' => $name_vi,
+                    'name_en' => $name_en,
+                    'status_id' => $status_id,
+                    'type_id' => $type_id,
+                    'is_active' => $is_active,
+                    'description_vi' => $description,
+                    'from_file' => "Sheet_" . $sheet_name . "_List & Database-SPC.xlsx"
+                );
+                // print_r($array);
+                $id = $document_model->insert($array);
+            }
+        }
+    }
     function updateqr()
     {
 
